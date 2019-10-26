7f57f19df3d098ce761797449068dd09f94ccbae
Dan Kennedy <dannykennedy@email.com>
2019-10-26 16:18:20 +1100

    put navbar back in app.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import "./App.css";
 import { connect } from "react-redux";
 import { fetchData as _fetchData } from "./ducks";
 // Components
-import Navbar from "./components/Navbar";
 import InputArea from "./components/InputArea";
 import ChartsArea from "./components/Chartsarea";
 import PostsArea from "./components/PostsArea";
 import { withAuth } from "@okta/okta-react";
 import { useAuth } from "./auth";
+import FontAwesome from "react-fontawesome";
+import "./components/css/Navbar.css";
 
 const App = withAuth(({ fetchData, auth }) => {
     // const { fetchData } = props;
 
     const [authenticated, user] = useAuth(auth);
+
+    if (user) {
+        console.log("We have a user", user);
+    }
 
     useEffect(() => {
         fetchData();
@@ -21,17 +26,27 @@ const App = withAuth(({ fetchData, auth }) => {
 
     return (
         <div className="App">
-            <Navbar siteTitle={"Dream Network"} />
-            {authenticated !== null && (
-                <button
-                    onClick={() =>
-                        authenticated ? auth.logout() : auth.login()
-                    }
-                    className="App-link"
-                >
-                    Log {authenticated ? "out" : "in"}
-                </button>
-            )}
+            <header id="navbar">
+                <div className="app-name">
+                    <FontAwesome name="cloud-moon" />
+                    <span id="navbar-site-title">Dream Network</span>
+                </div>
+                <div id="navbar-menu-items">
+                    <div className="navbar-icon">
+                        {authenticated !== null && (
+                            <button
+                                onClick={() =>
+                                    authenticated ? auth.logout() : auth.login()
+                                }
+                                className="App-link"
+                            >
+                                Log {authenticated ? "out" : "in"}
+                            </button>
+                        )}
+                    </div>
+                </div>
+            </header>
+
             <div id={"App-body"}>
                 <div id={"notes-area"}>
                     <InputArea />
