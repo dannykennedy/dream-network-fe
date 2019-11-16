e142a1810b52f13e2ed795829eb6c6744e1bcb37
Dan Kennedy <dannykennedy@email.com>
2019-11-16 14:07:27 +1100

    splitting up ducks

--- src/components/TagsArea.js
+++ src/components/TagsArea.js
@@ -7,96 +7,101 @@ import { connect } from "react-redux";
 import { addTagToCurrentlyEditingPost as _addTagToCurrentlyEditingPost } from "../ducks/posts";
 
 const KeyCodes = {
-  comma: 188,
-  enter: 13
+    comma: 188,
+    enter: 13,
 };
 
 const isEnterKey = keyCode => {
-  return keyCode === KeyCodes.enter || keyCode === KeyCodes.comma;
+    return keyCode === KeyCodes.enter || keyCode === KeyCodes.comma;
 };
 
 function TagsArea({
-  tags,
-  noteId,
-  editingPost,
-  addTagToCurrentlyEditingPost,
-  currentlyEditingPosts
+    tags,
+    noteId,
+    editingPost,
+    addTagToCurrentlyEditingPost,
+    currentlyEditingPosts,
 }) {
-  const [inputText, setInputText] = useState("");
-  const [cardTags, setCardTags] = useState(tags);
+    const [inputText, setInputText] = useState("");
+    const [cardTags, setCardTags] = useState(tags);
 
-  useEffect(() => {
-    setCardTags(tags);
-  }, [tags]);
+    useEffect(() => {
+        setCardTags(tags);
+    }, [tags]);
 
-  const onAddTag = tagName => {
-    const id = uuidV4();
-    const newTag = {
-      tagId: id,
-      tagName: tagName,
-      tagType: "OTHER",
-      noteId: noteId
+    const onAddTag = tagName => {
+        const id = uuidV4();
+        const newTag = {
+            tagId: id,
+            tagName: tagName,
+            tagType: "OTHER",
+            noteId: noteId,
+        };
+        setCardTags([...cardTags, newTag]);
+        addTagToCurrentlyEditingPost(newTag);
     };
-    setCardTags([...cardTags, newTag]);
-    addTagToCurrentlyEditingPost(newTag);
-  };
 
-  const onDeleteTag = tagId => {
-    setCardTags(cardTags.filter(tag => tag.tagId !== tagId));
-  };
+    const onDeleteTag = tagId => {
+        setCardTags(cardTags.filter(tag => tag.tagId !== tagId));
+    };
 
-  return (
-    <div className="card-footer">
-      {cardTags ? (
-        <div className={"tags-area"}>
-          {cardTags.map(tag => {
-            return (
-              <Tag
-                name={tag.tagName}
-                type={tag.tagType}
-                tagId={tag.tagId}
-                key={tag.tagId}
-                noteId={noteId}
-                editing={editingPost}
-                onDelete={onDeleteTag}
-              />
-            );
-          })}
-          {editingPost && (
-            <input
-              placeholder={"Add a tag"}
-              className={"add-tag-input"}
-              value={inputText}
-              onKeyDown={e => {
-                if (isEnterKey(e.keyCode)) {
-                  onAddTag(inputText);
-                  setInputText("");
-                }
-              }}
-              onChange={e => {
-                if (!(e.target.value === "," || e.target.value === "\n")) {
-                  setInputText(e.target.value);
-                }
-              }}
-            />
-          )}
+    return (
+        <div className="card-footer">
+            {cardTags ? (
+                <div className={"tags-area"}>
+                    {cardTags.map(tag => {
+                        return (
+                            <Tag
+                                name={tag.tagName}
+                                type={tag.tagType}
+                                tagId={tag.tagId}
+                                key={tag.tagId}
+                                noteId={noteId}
+                                editing={editingPost}
+                                onDelete={onDeleteTag}
+                            />
+                        );
+                    })}
+                    {editingPost && (
+                        <input
+                            placeholder={"Add a tag"}
+                            className={"add-tag-input"}
+                            value={inputText}
+                            onKeyDown={e => {
+                                if (isEnterKey(e.keyCode)) {
+                                    onAddTag(inputText);
+                                    setInputText("");
+                                }
+                            }}
+                            onChange={e => {
+                                if (
+                                    !(
+                                        e.target.value === "," ||
+                                        e.target.value === "\n"
+                                    )
+                                ) {
+                                    setInputText(e.target.value);
+                                }
+                            }}
+                        />
+                    )}
+                </div>
+            ) : (
+                <LoadingNotice loadingText="Tagging in progress!" />
+            )}
         </div>
-      ) : (
-        <LoadingNotice loadingText="Tagging in progress!" />
-      )}
-    </div>
-  );
+    );
 }
 
 // these parts of state are passed in as props
 const mapStateToProps = state => {
-  return {
-    currentlyEditingPosts: state.currentlyEditingPosts
-  };
+    return {
+        currentlyEditingPosts: state.posts.currentlyEditingPosts,
+    };
 };
 
 const mapDispatchToProps = {
-  addTagToCurrentlyEditingPost: _addTagToCurrentlyEditingPost
+    addTagToCurrentlyEditingPost: _addTagToCurrentlyEditingPost,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(TagsArea);
